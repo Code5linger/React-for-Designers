bf55b26f21c4cb60e4b769f3e332dee4aebe5816
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-07-14 21:04:20 +0600

    End of Video 2

--- v0.0/src/pages/index.js
+++ v0.0/src/pages/index.js
@@ -3,9 +3,14 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Learn to Design & Code React apps</h1>
-    <p>Complete courses about the best tools and design systems. Prototyping and build apps with React and Swift.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to Design & Code React apps</h1>
+        <p>Complete courses about the best tools and design systems. Prototyping and build apps with React and Swift.</p>
+        <Link to="/page-2/">Watch the video</Link>
+      </div>
+    </div>
+
   </div>
 )
 
