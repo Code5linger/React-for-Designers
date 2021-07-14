e4933e854dcc4e92d51ac4283986977edb2a2b6c
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-07-14 23:38:21 +0600

    End of Video 04

--- v0.0/src/pages/index.js
+++ v0.0/src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <div className="Hero">
       <div className="HeroGroup">
-        <h1>Learn to Design & Code React apps</h1>
+        <h1>Learn to <br/>Design & Code React apps</h1>
         <p>Complete courses about the best tools and design systems. Prototyping and build apps with React and Swift.</p>
         <Link to="/page-2/">Watch the video</Link>
       </div>
