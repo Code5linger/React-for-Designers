3a2d7d8d1ab50c04555212d987a13c2ffcc0988c
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-07-29 20:56:54 +0600

    v4.0 3/12

--- v4.0/src/components/header.js
+++ v4.0/src/components/header.js
@@ -1,31 +1,19 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './Header.css'
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div className="Header">
+    <div className="HeaderGroup">
+      <Link to="/">
+        <div className="logo">
+          Omega
+        </div>
+      </Link>
+      <Link to="/demos">Demos</Link>
+      <Link to="/pages">Pages</Link>
+      <Link to="/support">Support</Link>
+      <Link to="/access"><button>Get Beta Access</button></Link>
     </div>
   </div>
 )
