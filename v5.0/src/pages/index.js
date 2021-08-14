1cd27d87b199fb69a4cc25e329e75d65cb27ba88
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-08-14 17:36:53 +0600

    14/08 3/12

--- v5.0/src/pages/index.js
+++ v5.0/src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Form from '../images/Form.png'
 
 const IndexPage = () => (
   <div>
@@ -7,6 +8,7 @@ const IndexPage = () => (
       <div className="HeroGroup">
         <h1>Build your next landing page in 5 minutes!</h1>
         <p>Create custom landing pages with Omega that convert more visitors than any website. With lots of unique blocks, you can easily build a page without coding.</p>
+        <img src={Form} alt="Form" srcset="../images/Form.png" />
         <Link to="/page-2/">Get Beta Access</Link>
       </div>
     </div>
