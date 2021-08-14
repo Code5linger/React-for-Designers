8908ec9c979d2e37992dbccbe2926d999c1bde3a
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-08-14 18:34:19 +0600

    14-08 Grinding on

--- v5.0/src/pages/index.js
+++ v5.0/src/pages/index.js
@@ -6,10 +6,18 @@ const IndexPage = () => (
   <div>
     <div className="Hero">
       <div className="HeroGroup">
-        <h1>Build your next landing page in 5 minutes!</h1>
-        <p>Create custom landing pages with Omega that convert more visitors than any website. With lots of unique blocks, you can easily build a page without coding.</p>
-        <img src={Form} alt="Form" srcset="../images/Form.png" />
-        <Link to="/page-2/">Get Beta Access</Link>
+        <div className="Wrapper">
+          <div className="ContentWrapper">
+          <div className="TextWrapper">
+            <h1>Build your next landing page in 5 minutes!</h1>
+              <p>Create custom landing pages with Omega that convert more visitors than any website. With lots of unique blocks, you can easily build a page without coding.</p>
+              <Link to="/page-2/">Get Beta Access</Link>
+            </div>
+          </div>
+          <div className="Form">
+          <img src={Form} alt="Form" srcset="../images/Form.png" />
+          </div>
+        </div>
       </div>
     </div>
   </div>
