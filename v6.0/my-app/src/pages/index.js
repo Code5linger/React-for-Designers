dd5644dab15171dc28e0b333deb747f596ef4356
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-08-15 23:17:52 +0600

    15-08

--- v6.0/my-app/src/pages/index.js
+++ v6.0/my-app/src/pages/index.js
@@ -1,18 +1,24 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout/layout"
-import SEO from "../components/layout/seo"
 import images from "../images/Form.png"
+import "../pages/index.css"
 
 function IndexPage() {
   return (
     <Layout>
-      <SEO title="Home" />
-      <img src="/images/logos/logo.svg" alt="logo" />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <img src={images} alt="Form" srcset="" />
+      <div className="TextContent">
+        <h1>Build your next landing page in 5 minutes!</h1>
+        <p>
+          Create custom landing pages with Omega that convert more visitors than
+          any website. With lots of unique blocks, you can easily build a page
+          without coding.
+        </p>
+        <Link to="/page-2/">Get Beta Access</Link> <br />
+      </div>
+      <div className="ImageContent">
+        <img src={images} alt="Form" srcset="" />
+      </div>
     </Layout>
   )
 }
